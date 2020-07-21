bf3b61d9d0d3f125012b8745e4089fe02c1ce435
vamsijavvadi7 <59219320+vamsijavvadi7@users.noreply.github.com>
2020-07-21 20:12:08 +0530

    Add files via upload

---
+++ TodoForm.js
@@ -0,0 +1,39 @@
+import React,{useState} from 'react';
+import uuid from "uuid";
+import {Button,TextField} from '@material-ui/core';
+function TodoForm({addTodo}){
+   const [todo,setTodo]=useState({
+id:"",
+task:"",
+completed:false
+});
+function handleTaskInputChange(e){
+setTodo({...todo,task : e.target.value});
+
+}
+function handleSubmit(e){
+    e.preventDefault();
+    if(todo.task.trim()){
+        addTodo({...todo,id:uuid.v4()}); 
+    setTodo({...todo,task:""});
+    }
+
+}
+
+
+    return(
+<form classname="todo-form" onSubmit={handleSubmit}>
+    <TextField 
+    label="Task"
+    name="task"
+    type="text"
+    value={todo.task}
+    onChange={handleTaskInputChange}
+    />
+    <Button type="submit">Submit</Button>
+</form>
+
+
+    );
+}
+export default TodoForm;
